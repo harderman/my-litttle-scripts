45fd893d3464f2d74ff0b99aaa80cbc4df1a7bbd
Wanghao Xu <echoxwh@163.com>
2019-04-16 20:12:06 +0800

    add img_binary_inv script

--- img_binary_inv.py
+++ img_binary_inv.py
@@ -7,10 +7,10 @@ import cv2
 
 old_img_path = "./fig/IMG_2952.JPG"
 
-img = cv2.imread(old_img_path)
+img = cv2.imread(old_img_path, 0)
 
 new_img_path = "./fig/gray.jpg"
 
-gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, thresh2 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
 
-cv2.imwrite(new_img_path, gray_image)
+cv2.imwrite(new_img_path, thresh2)

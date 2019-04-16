244300b1ebf0fbd49e77daac86981276f6f41424
Wanghao Xu <echoxwh@163.com>
2019-04-16 20:13:10 +0800

    add img_binary_inv script

--- img_binary_inv.py
+++ img_binary_inv.py
@@ -9,7 +9,7 @@ old_img_path = "./fig/IMG_2952.JPG"
 
 img = cv2.imread(old_img_path, 0)
 
-new_img_path = "./fig/gray.jpg"
+new_img_path = "./fig/binary_inv.jpg"
 
 ret, thresh2 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
 
